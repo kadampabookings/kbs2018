e8b93b80c78c934814bb84ccc5e767f210628a34
Bruno Salmon <dev.salmonb@gmail.com>
2023-10-07 19:46:35 +0100

    Split i18n dictionaries inside modules

--- kbsx-apps/kbsx-frontoffice-application-gwt/src/main/resources/super/kbsx/frontoffice/application/gwt/embed/EmbedResourcesBundle.java
+++ kbsx-apps/kbsx-frontoffice-application-gwt/src/main/resources/super/kbsx/frontoffice/application/gwt/embed/EmbedResourcesBundle.java
@@ -15,22 +15,22 @@ public interface EmbedResourcesBundle extends ClientBundle {
     @Source("dev/webfx/platform/meta/exe/exe.properties")
     TextResource r2();
 
-    @Source("images/svg/color/price-tag.svg")
+    @Source("dev/webfx/stack/i18n/en.properties")
     TextResource r3();
 
-    @Source("images/svg/mono/calendar.svg")
+    @Source("dev/webfx/stack/i18n/fr.properties")
     TextResource r4();
 
-    @Source("images/svg/mono/certificate.svg")
+    @Source("images/svg/color/price-tag.svg")
     TextResource r5();
 
-    @Source("images/svg/mono/price-tag.svg")
+    @Source("images/svg/mono/calendar.svg")
     TextResource r6();
 
-    @Source("one/modality/base/client/services/i18n/dictionaries/en.json")
+    @Source("images/svg/mono/certificate.svg")
     TextResource r7();
 
-    @Source("one/modality/base/client/services/i18n/dictionaries/fr.json")
+    @Source("images/svg/mono/price-tag.svg")
     TextResource r8();
 
     @Source("one/modality/base/shared/domainmodel/DomainModelSnapshot.json")
@@ -42,12 +42,12 @@ public interface EmbedResourcesBundle extends ClientBundle {
         public ProvidedGwtResourceBundle() {
             registerResource("dev/webfx/platform/conf/src-root.properties", R.r1());
             registerResource("dev/webfx/platform/meta/exe/exe.properties", R.r2());
-            registerResource("images/svg/color/price-tag.svg", R.r3());
-            registerResource("images/svg/mono/calendar.svg", R.r4());
-            registerResource("images/svg/mono/certificate.svg", R.r5());
-            registerResource("images/svg/mono/price-tag.svg", R.r6());
-            registerResource("one/modality/base/client/services/i18n/dictionaries/en.json", R.r7());
-            registerResource("one/modality/base/client/services/i18n/dictionaries/fr.json", R.r8());
+            registerResource("dev/webfx/stack/i18n/en.properties", R.r3());
+            registerResource("dev/webfx/stack/i18n/fr.properties", R.r4());
+            registerResource("images/svg/color/price-tag.svg", R.r5());
+            registerResource("images/svg/mono/calendar.svg", R.r6());
+            registerResource("images/svg/mono/certificate.svg", R.r7());
+            registerResource("images/svg/mono/price-tag.svg", R.r8());
             registerResource("one/modality/base/shared/domainmodel/DomainModelSnapshot.json", R.r9());
 
         }
