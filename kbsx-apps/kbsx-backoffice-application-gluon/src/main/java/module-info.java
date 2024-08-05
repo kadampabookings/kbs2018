94e0d748286e3ee5567d6d00b646d8f97a6b6fdf
Bruno Salmon <dev.salmonb@gmail.com>
2024-08-05 13:04:46 +0100

    Ran 'webfx update' (removed SSO logins)

--- kbsx-apps/kbsx-backoffice-application-gluon/src/main/java/module-info.java
+++ kbsx-apps/kbsx-backoffice-application-gluon/src/main/java/module-info.java
@@ -26,9 +26,6 @@ module kbsx.backoffice.application.gluon {
     requires webfx.platform.windowhistory.java;
     requires webfx.platform.windowlocation.java;
     requires webfx.stack.authn.buscall;
-    requires webfx.stack.authn.login.buscall;
-    requires webfx.stack.authn.login.remote;
-    requires webfx.stack.authn.login.ui.gateway.webviewbased.openjfx;
     requires webfx.stack.authn.login.ui.portal;
     requires webfx.stack.authn.remote;
     requires webfx.stack.com.bus.json.client;
