6c451ac3f81c7ff6926f9ed95e7dd5af39fbc69e
Bruno Salmon <dev.salmonb@gmail.com>
2024-08-18 22:55:37 +0100

    Ran 'webfx update'

--- kbsx-apps/kbsx-backoffice-application-openjfx/src/main/java/module-info.java
+++ kbsx-apps/kbsx-backoffice-application-openjfx/src/main/java/module-info.java
@@ -27,10 +27,10 @@ module kbsx.backoffice.application.openjfx {
     requires webfx.platform.shutdown.java;
     requires webfx.platform.storage.java;
     requires webfx.platform.storagelocation.java;
+    requires webfx.platform.useragent.java.client;
     requires webfx.platform.windowhistory.java;
     requires webfx.platform.windowlocation.java;
     requires webfx.stack.authn.buscall;
-    requires webfx.stack.authn.login.ui.portal;
     requires webfx.stack.authn.remote;
     requires webfx.stack.com.bus.json.client;
     requires webfx.stack.com.bus.json.client.websocket.java;
